5c9ae1bac335f47178fc823b5cbce7f4d08b19f3
pabsan16 <124245920+pabsan16@users.noreply.github.com>
2023-05-16 21:27:38 +0200

    Add files via upload

---
+++ Tric3.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+TERCER APARTADO
+
+PABLO SÁNCHEZ RICO
+"""
+
+import sys
+from pyspark import SparkContext
+
+
+def mapp(line):
+    nodes = line.split(' , ')
+    if nodes[0] < nodes[1]:
+        return (nodes[0], nodes[1])
+    elif nodes[1] < nodes[0]:
+        return (nodes[1], nodes[0])
+    else:
+        pass
+    
+    
+def adj_list(nlist):
+    node = nlist[0]
+    l = nlist[1]
+    m = len(l)
+    ad_list = []
+    for e in range(m):
+        a = l[e]
+        ad_list.append(((node, a), 'exists'))
+        for k in range(e+1, m):
+            ad_list.append(((a, l[k]), ('pending', node)))
+    return ad_list
+
+
+def get_tric(tric):
+    ftrics = []
+    for par, msn in tric.collect():
+        list_msn = list(msn)
+        if len(list_msn) > 1 and 'exists' in list_msn:
+            for i in list_msn:
+                if i != 'exists':
+                    ftrics.append((i[1], par[0], par[1]))
+    return sorted(ftrics)
+
+
+def main(sc, list_file):
+    """
+    Recorremos la lista de ficheros, para cada fichero creamos 
+    el grafo mapeando y eliminando duplicados. Después creamos 
+    la lista de nodos adyacentes ordenando por Key, obtenemos 
+    los triciclos y luego imprimimos la lista resultante.
+    """
+    for file in list_file:
+        g = sc.textFile(file)
+        gra = g.map(mapp).distinct().filter(lambda x: x!=None)
+        ady = gra.groupByKey().map(lambda x: (x[0], sorted(list(x[1])))).sortByKey()
+        tric = ady.flatMap(adj_list).groupByKey()
+        print ('File ', file, ' has  these tricycles: ', get_tric(tric))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Uso: python3 {0} <file list>'.format(sys.argv[0]))
+    else:
+        with SparkContext() as sc:
+            sc.setLogLevel('ERROR')
+            main(sc, sys.argv[1:])
+
+ 
